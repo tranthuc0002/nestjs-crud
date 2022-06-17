983f8712e4d37e410f78e483e2eb48fc8850c24e
unknown <tran.thuc00002@gmail.com>
2022-06-17 10:41:28 +0700

    modify validate pipe

--- src/core/pipes/validate.pipe.ts
+++ src/core/pipes/validate.pipe.ts
@@ -2,17 +2,18 @@ import { Injectable, ArgumentMetadata, BadRequestException, ValidationPipe, Unpr
 
 @Injectable()
 export class ValidateInputPipe extends ValidationPipe {
-   public async transform(value, metadata: ArgumentMetadata) {
-      try {
-        return await super.transform(value, metadata);
-      } catch (e) {
-         if (e instanceof BadRequestException) {
-            throw new UnprocessableEntityException(this.handleError(e.message));
-         }
-      }
-   }
+    public async transform(value, metadata: ArgumentMetadata) {
+        try {
+            return await super.transform(value, metadata);
+        } catch (e) {
+            var temp = e.response;
+            if (e instanceof BadRequestException) {
+                throw new UnprocessableEntityException(this.handleError(temp));
+            }
+        }
+    }
 
-   private handleError(errors) {
-        return (error => error.constraints);
-   }
+    private handleError(errors) {
+        return errors;
+    }
 }
